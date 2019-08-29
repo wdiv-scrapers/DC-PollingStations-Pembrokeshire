e2c8fd6e16d1d57eaa3899bc327fa20dcf2e2b1e
chris48s <chris.shaw480@gmail.com>
2019-08-29 14:46:38 +0100

    update URLs

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
 from dc_base_scrapers.geojson_scraper import GeoJsonScraper
 
 
-stations_url = "http://www.pembrokeshire.gov.uk/geoserver/maps/live/wms?service=WFS&version=1.3.0&request=GetFeature&typeNames=PCC%3AtblPollingStation&outputFormat=json&srsName=EPSG%3A4326"
-districts_url = "http://www.pembrokeshire.gov.uk/geoserver/maps/live/wms?service=WFS&version=1.3.0&request=GetFeature&typeNames=PCC%3AtblPollingDistrict&outputFormat=json&srsName=EPSG%3A4326"
+stations_url = "http://inspire.pembrokeshire.gov.uk/geoserver/wms?service=WFS&version=1.3.0&request=GetFeature&typeNames=PCC%3AtblPollingStation&outputFormat=json&srsName=EPSG%3A4326"
+districts_url = "http://inspire.pembrokeshire.gov.uk/geoserver/wms?service=WFS&version=1.3.0&request=GetFeature&typeNames=PCC%3AtblPollingDistrict&outputFormat=json&srsName=EPSG%3A4326"
 council_id = 'W06000009'
 
 
