317091212342975034d6f2be000d9868c78bee8b
GeoWill <roper.william@gmail.com>
2021-03-25 12:43:38 +0000

    Update council id

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from dc_base_scrapers.geojson_scraper import GeoJsonScraper
 
 stations_url = "http://inspire.pembrokeshire.gov.uk/geoserver/wms?service=WFS&version=1.3.0&request=GetFeature&typeNames=PCC%3AtblPollingStation&outputFormat=json&srsName=EPSG%3A4326"
 districts_url = "http://inspire.pembrokeshire.gov.uk/geoserver/wms?service=WFS&version=1.3.0&request=GetFeature&typeNames=PCC%3AtblPollingDistrict&outputFormat=json&srsName=EPSG%3A4326"
-council_id = 'W06000009'
+council_id = 'PEM'
 
 
 stations_scraper = GeoJsonScraper(stations_url, council_id, 'utf-8', 'stations')
